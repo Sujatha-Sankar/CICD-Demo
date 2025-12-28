dfbe04c641e62d49440b1202424c6122d7b02eaa
Sujatha-Sankar <sujasankar1994.com>
2025-12-28 09:09:57 +0530

    Initial my Flask app

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def health():
+    return jsonify({"status": "ok"})
+
+@app.route("/hello")
+def hello():
+    return jsonify({"message": "Hello from CI/CD"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
